27d6965f58ac611ab004b75944a292b00db9fe73
Ata Mahmoud <ata.ata250@hotmail.com>
2018-09-11 23:44:11 +0200

    Get method impelemntation

--- index.js
+++ index.js
@@ -23,6 +23,17 @@ const genres = [{
     },
 ];
 
+app.get(genresEndPoint, (req, res) => {
+    res.send(genres);
+});
+
+app.get(`${genresEndPoint}/:id`, (req, res) => {
+    const genr = genres.find(g => g.id === parseInt(req.params.id));
+    if (!genr) return res.status(404).send(`the genr with id: ${req.body.id} can't be found`);
+
+    res.send(genr);
+});
+
 
 const port = process.env.PORT || 4000;
 app.listen(port, () => console.log(`Lestining to port ${port}.....`));
