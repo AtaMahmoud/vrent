9746be3c030f403bcdb56022bd3f856d509f1842
Ata Mahmoud <ata.ata250@hotmail.com>
2018-09-21 02:33:46 +0200

    add Fawn Package to implement Two Phase Commit

--- routes/rentals.js
+++ routes/rentals.js
@@ -1,52 +1,74 @@
-const express=require('express');
-const router=express.Router();
-const {Rental,validate}=require('../models/rental');
-const {Customer}=require('../models/customer');
-const {Movie}=require('../models/movie');
+const express = require('express');
+const router = express.Router();
+const {
+    Rental,
+    validate
+} = require('../models/rental');
+const {
+    Customer
+} = require('../models/customer');
+const {
+    Movie
+} = require('../models/movie');
+const Fawn = require('fawn');
+const mongoose = require('mongoose');
 
-router.get('/',async (req,res)=>{
-    const rental=await Rental.find().sort('-dateOut');
+Fawn.init(mongoose);
+
+router.get('/', async (req, res) => {
+    const rental = await Rental.find().sort('-dateOut');
     res.send(rental);
 });
 
-router.post('/',async(req,res)=>{
-    const {error}=validate(req.body);
-    if(error) return res.status(400).send(error.details[0].message);
+router.post('/', async (req, res) => {
+    const {
+        error
+    } = validate(req.body);
+    if (error) return res.status(400).send(error.details[0].message);
 
-    const customer=await Customer.findById(req.body.customerId);
-    if(!customer) return res.status(400).send('Invalid Cutomer');
+    const customer = await Customer.findById(req.body.customerId);
+    if (!customer) return res.status(400).send('Invalid Cutomer');
 
-    const movie=await Movie.findById(req.body.movieId);
-    if(!movie) return res.status(400).send('Invalid movie');
+    const movie = await Movie.findById(req.body.movieId);
+    if (!movie) return res.status(400).send('Invalid movie');
 
-    if(movie.numberInStocke===0) return res.status(400).send('Movie not in Stock');
+    if (movie.numberInStocke === 0) return res.status(400).send('Movie not in Stock');
 
-    let rental=new Rental({
-        customer:{
-           _id:customer._id,
-           name:customer.name,
-           isGold:customer.isGold,
-           phoneNumber:customer.phoneNumber
+    let rental = new Rental({
+        customer: {
+            _id: customer._id,
+            name: customer.name,
+            isGold: customer.isGold,
+            phoneNumber: customer.phoneNumber
         },
-        movie:{
-            _id:movie._id,
-            title:movie.title,
-            dailytRentalRate:movie.dailytRentalRate
+        movie: {
+            _id: movie._id,
+            title: movie.title,
+            dailytRentalRate: movie.dailytRentalRate
         }
     });
 
-    rental=await rental.save();
+    try {
+        new Fawn.Task()
+            .save('rentals', rental)
+            .update('movies', {
+                _id: movie._id
+            }, {
+                $inc: {
+                    numberInStocke: -1
+                }
+            }).run();
 
-    movie.numberInStocke--;
-    movie.save();
+        res.send(rental);
+    } catch (error) {
+        res.send('Something happend');
+    }
+});
+
+router.get('/:id', async (req, res) => {
+    const rental = await Rental.findById(req.params.id);
+    if (!rental) return res.status(404).send(`The rental with the ID: ${req.params.id} was not found.`);
 
     res.send(rental);
 });
-
-router.get('/:id',async(req,res)=>{
-    const rental=await Rental.findById(req.params.id);
-    if(!rental) return res.status(404).send(`The rental with the ID: ${req.params.id} was not found.`);
-
-    res.send(rental);
-});
-module.exports=router;
+module.exports = router;
