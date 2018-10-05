e63ec7adcc8cfdac6c95bed660bfada97fd8f5e0
Ata Mahmoud <ata.ata250@hotmail.com>
2018-10-05 20:20:43 +0200

    Extracting config loggic

--- index.js
+++ index.js
@@ -1,19 +1,17 @@
 const express = require('express');
 const app = express();
 const Joi = require('joi');
-const config=require('config');
+
 Joi.objectId=require('joi-objectid')(Joi);
 
 require('./startup/logging')();
+require('./startup/config')();
 require('./startup/routes')(app);
 require('./startup/db')();
 
 
 
-if(!config.get('jwtPrivateKey')){
-    console.log('FATAL ERORR: jwtPrivateKey not defiend');
-    process.exit(1);
-}
+
 
 const port = process.env.PORT || 3000;
 app.listen(port, () => console.log(`Lestining to port ${port}.....`));
