8e2c5c6ecad0b893893b0c435830e324f6ec5a2f
Ata Mahmoud <ata.ata250@hotmail.com>
2018-10-13 22:29:41 +0200

    setup app for integration and db test

--- startup/db.js
+++ startup/db.js
@@ -1,9 +1,11 @@
 const mongoose = require('mongoose');
 const winston=require('winston');
+const config=require('config');
 
 module.exports = function () {
-    mongoose.connect('mongodb://localhost/vrent', {
+    const db=config.get('db');
+    mongoose.connect(db, {
             useNewUrlParser: true
         })
-        .then(() => winston.info('Connected to mongodb'));
+        .then(() => winston.info(`Connected to ${db} `));
 }
